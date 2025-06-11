801b1f99390fdfaffce9e0dc2818587a6b6ab176
Mario Martinovic <mariomartinovic2001@gmail.com>
2025-06-11 14:49:48 +0200

    07-Back-to-top-btn features: implement btn-to-top

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const burgerMenuElement = document.querySelector("#burger-menu");
 
 const navLinksElement = document.querySelector("#nav-links");
+const scrollToTopElement = document.querySelector("#scrollToTopBtn");
 
 burgerMenuElement.addEventListener("click", () => {
   navLinksElement.classList.toggle("active");
+});
+
+scrollToTopElement.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 });
